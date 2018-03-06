df8893cfdf5666e4282c87580d24c20632c30dfd
barttomczyk-coderslab <bartlomiej.tomczyk@coderslab.pl>
2018-03-06 19:49:08 +0100

    Update app.js

--- js/app.js
+++ js/app.js
@@ -14,11 +14,22 @@ document.addEventListener("DOMContentLoaded", function() {
         var newContent = content.value;
 
         //tworzy się div, h1 się nie tworzy
+        /* --- answer ---
+           pamiętaj, że po stworzeniu elementu i przypisaniu do zmiennej, musimy go dodać dod DOM.
+           zrobiłaś to w linijce newDiv.appendChild(newNote);
+        */
         var newNote = document.createElement("div");
         var newH1 = document.createElement("h1");
 
         //tworzy się div z tekstem, ale w jednym ciągu, bez podziału na tytuł i treść notatki-chciałabym to jakoś podzielić
+        /* --- answer ---
+           myśle ze odpowiednia kolejność dodawania zalątwi sprawę
+           jak na razie h1 nie dodaje do DOM
+        */
         //wpisywana treść wychodzi poza diva
+        /* --- answer ---
+           zobacz jak zawijać tekst w css, przyda sie wlasnosc text-wrap
+        */
         if (content.value.length >= 1 && content.value.length <=50) {
             newDiv.appendChild(newNote);
             newNote.id = "draggable";
@@ -34,6 +45,10 @@ document.addEventListener("DOMContentLoaded", function() {
         }
 
         // draggable działa tylko na pierwszym utworzonym elemencie
+        /* --- answer ---
+           zgadza się :)
+            bo w momencie inicjowania draggable() masz tylko jeden element - powinnaś inicjować bibliotekę za każdym razem
+        */
         $(function() {
             $( "#draggable" ).draggable();
 
@@ -47,6 +62,9 @@ document.addEventListener("DOMContentLoaded", function() {
         remButton.addEventListener("click", function () {
 
             //usuwa notatkę, ale button "Delete" zostaje, chciałabym żebby usuwał się razem z notatką
+            /* --- answer ---
+           za na pewno ten button jest wewnątrz tego diva?
+             */
             newNote.parentElement.removeChild(newNote);
 
 
@@ -56,5 +74,5 @@ document.addEventListener("DOMContentLoaded", function() {
     });
 
 });
-
+// Drobna wskazówka - ogólnie piszesz w czystym js, moze łątwiej będzie w jquery, skoro już podpięłaś tą bibliotekę?
 
